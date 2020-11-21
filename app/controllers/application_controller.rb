4c5a8041b6720c98cfe5fb2935d4b6ea9c354f4f
Natalia <hourglassbranch25@gmail.com>
2020-11-21 04:55:25 -0300

    grade

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
 
     def authorize_admin
-        unlesss current_user.admin?
-        redirect_to wines_path, notice: "You must be an admin to access this section"
+        unless current_user.admin?
+            redirect_to wines_path, notice: "You must be an admin to access this section"
+        end
     end
 
 end
--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,7 +1,7 @@
 class Wine < ApplicationRecord
     has_many :wine_strains, dependent: :destroy
     has_many :strains, through: :wine_strains
-    has_many :enologist_wines
+    has_many :oenologist_wines
     has_many :oenologists, through: :oenologist_wines
 
 end
